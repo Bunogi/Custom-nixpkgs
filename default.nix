c3db71355f1cfade5b9fd797f159776b4538bac7
Håkon Jordet <haakon.jordet@gmail.com>
2020-03-23 08:55:40 +0100

    Add emacs with pdf-tools

--- default.nix
+++ default.nix
@@ -2,6 +2,9 @@
   overlay = self: super: {
     screentool = super.callPackage ./packages/screentool {};
     volume = super.callPackage ./packages/volume {};
+    emacs = super.emacsPackages.emacsWithPackages (em: (with em;
+      [ pdf-tools ]
+    ));
   };
 
   config = import ./config;
