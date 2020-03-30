b78d06540667405ab1807a2049531ac32e1e05ca
Håkon Jordet <haakon.jordet@gmail.com>
2020-03-30 14:30:37 +0200

    Add laptop ssh key

--- default.nix
+++ default.nix
@@ -2,9 +2,6 @@
   overlay = self: super: {
     screentool = super.callPackage ./packages/screentool {};
     volume = super.callPackage ./packages/volume {};
-    emacs = super.emacsPackages.emacsWithPackages (em: (with em;
-      [ pdf-tools ]
-    ));
     botty = super.callPackage ./packages/botty {};
     monshot = super.callPackage ./packages/monshot {};
   };
@@ -18,5 +15,6 @@
 
   sshKeys = [
       "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC4nkyU90FdeM+jhuiRAoKDoUVSukTWobOkrQgiZgojH1zwOppc0Y4Exu1i8o22MIrC9Ef8J74baOccEWbNkvM1f915FGDdkQYjOUCvDC6drZLpAPf5Ps4J50EgjdUjb2t7wSFdMpkUITfAYjvTYiGjTzaXEdXuY6C2VG59m7MiclzrowV3pPnjCZfw9gFBTV3Rz+p69ctVH418jMujIZriHT0HbAIiNwy8GR0cQZPpXBsZflMOHEIXdtUDtAf8b9GLVZ5Bl/mNNfD85Ybf5KDHWPZ8BE+LOTD4GpvQ5OprvdZTn3YkzYl+drb7wIa94ZnNyyxrnIl5g/wljLLMEvVDNusoPx0aKSDtm5Y0hQ6SSQEaafaGyrqRkKyTtkkX1Fx9Pa90taXxoWBK1l/2EGhTU5nG2XdjzT7AOko4nY2iiKzof0OF/nokOVjLEss5UH6IYf8YIzQV9H2YUYqBEbFbtlb23qArhF68NcUDMaN3laUIQ7Y7DcQAYfxVgu8AtyH5AdSpOIGAnYZaICTPVy7O96XE7oPKPFPg+jjm6vEcUwcvZ+ns5+sBayZiTHM5HUDMyQHNH0Ue7vnHeZ+un/QdVERADpLiga/3sQedQiw5xULp4FEJ66jmZQeIGXUPvB3OLkscycWWq9t1neizef9L9xj4Q3ZTG0UB7OE3g6AOmQ== bunogi@zenix"
+      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCtlTW1/iP30h+Ziaz8qq+OQggnyem20cuAWUZPRy2L1YqM1Xpw41ylyoaLxxDJjHSIhjfy6j1Zn43Ms53Xpe7GWk+l/VNr0KwApeU44SvARFqE10/c+0YcrnTEBqhSvChEcjFs/AJ0AEIVB9spi8citf71/KgjGkfd8TvX0nNNUdz26pvjFRf5bjIL6m2zC3dL56GbiflhQ/Ft+UOt6+xRXieYdGkW37X9jzkgcUqKOnKZkXY39t1wZCeh6gMFce7RH9DBcqeyJuuSjNuK8vmx88P3hAyvmzfZmPU63BgKJHV1m54Aha3qyVU2tesw0ZRMlf8jpw0JZY727H13fKPhNM846udn7l/Y1NDA7C635TQY5zsnqBgk7j7flVXZzMTya0zY3FcRxI169M1Fjd56DhFLar46ttKJHNY6RwePVHmJVkb42/Crto3u5/iiQ/D9rbObWXF1nI9OqZOJCtvMZaptwmFQ1Oh7omtH94htOQAZWPYOj/bvxn/2DZhvwkI0H6gM0PpsfBDMDoE3O8P3+B5ZNVZLNnN/rI+mfyJabUbeDuKe3ABWNGmOuT1QcTKxVr57Axo3F8T8fCEWaid76tXK46cXvF+qe7Kw4Il0f/y5FsoXVE50E1TULzEoUNB2pPTh6AlGqYe3DloIwiswzNpkTFUmNnCrenbL5FRbQQ== bunogi@archzen"
     ];
 }
