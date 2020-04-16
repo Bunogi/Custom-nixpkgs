7e35f054f2b795c940575ca2f00ede45956d6e08
Håkon Jordet <haakon.jordet@gmail.com>
2020-04-16 11:22:28 +0200

    Update monshot Rust nightly

--- packages/monshot/default.nix
+++ packages/monshot/default.nix
@@ -6,21 +6,21 @@ let
   nixpkgs = import (fetchTarball https://github.com/NixOS/nixpkgs-channels/archive/nixos-unstable.tar.gz) {
     overlays = [ moz_overlay ];
   };
-  nightly = nixpkgs.rustChannelOf { date = "2020-04-06"; channel = "nightly"; };
+  nightly = nixpkgs.rustChannelOf { date = "2020-04-16"; channel = "nightly"; };
 in
 with nixpkgs;
   rustPlatform.buildRustPackage.override {
     rustc = nightly.rust;
   } rec {
     name = "monshot-${version}";
-    version = "0.1.3";
+    version = "0.1.4";
 
     src = builtins.fetchGit {
       url = "https://git.bunogi.xyz/bunogi.xyz/monshot";
       rev = "80b74ee52ee10ac2ed61dd712d0a089cbd03c33e";
     };
 
-    cargoSha256 = "0454ixc03agify1v377c0nwf60g10pgz6f0gnb49838gb8jwzbcj";
+    cargoSha256 = "0nn4rzhsja5w63pin8sdpypbs7mxpbkxiwa2mhd9ja6haa9ymwaw";
 
     # builder = ./builder.sh;
 
